3f08bd455c2fa18245d3e71f76cf8a0adaf3670c
HarenosWebDev <gauthier@gauthierdaniels.fr>
2022-01-20 17:24:10 +0100

    on a maintenant deux cartes par fruits, pour 28 cartes au total

--- src/js/MemoryGameEngine.js
+++ src/js/MemoryGameEngine.js
@@ -17,8 +17,14 @@ class MemoryGameEngine {
 	 * Gestion de la création des cartes pour la nouvelle grille de jeu.
 	 */
 	createCards() {
-    // Pour chaque fruit, nous créons une carte
-		this.fruits.forEach(fruit => {
+		// Ici, on crée un tableau avec deux fois les valeurs contenues
+		// dans la propriété `this.fruits`.
+		// Cela nous permet d'avoir deux fois chaque fruit !
+		// Par contre, ils ne sont pas mélangés entre eux, on va donc
+		// utiliser notre prototype sur les arrays pour mélanger
+		// ce joli tableau pour éviter que les cartes soient toujours
+		// aux mêmes positions sur la grille de jeu.
+		[...this.fruits, ...this.fruits].shuffle().forEach(fruit => {
       // Ici, on gère l'instanciation d'une nouvelle carte pour
       // le fruit qui lui a été donné...
 			const newCard = new MemoryCard(fruit);
