2a1d1a92733263dce9655d14601e98cbf27ce698
HarenosWebDev <gauthier@gauthierdaniels.fr>
2022-01-20 19:04:40 +0100

    on crée un nouveau controller pour les scores

---
+++ controllers/Score.js
@@ -0,0 +1,13 @@
+const Controller = require('./Controller');
+
+class ScoreController extends Controller {
+	constructor(req, res) {
+		super(req, res);
+	}
+
+	async scoreSubmit() {
+
+	}
+}
+
+module.exports = ScoreController;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const GlobalController = require("../controllers/Global");
+const ScoreController = require("../controllers/Score");
+
+// Chacune de nos routes appelleront des méthodes de nos controllers,
+// après avoir fait une nouvelle instance (`new GlobalController(req, res)` par exemple).
+// Dans le constructeur de nos controllers, on souhaite attribuer des propriétés selon
+// deux informations fournies à l'instanciation :
+// - La requête (req)
+// - La réponse (res)
+// On n'oublie donc pas de les fournir !
 
 const routes = (app) => {
 	// Cette première route retournera la vue de notre application.
 	app.get('/', (req, res) => new GlobalController(req, res).sendHome());
+	// Celle-ci est un endpoint pour notre API.
+	// Elle permet l'enregistrement de nouveaux scores.
+	app.post('/scoreSubmit', (req, res) => new ScoreController(req, res).scoreSubmit());
 };
 
 module.exports = routes;
